1ff15f1d0900e98058b01583359143cc36ca4ce6
blitzik <ales.tichava@gmail.com>
2020-06-30 16:45:19 +0200

    save

--- lib/utils/result_object/animation_utils.dart
+++ lib/utils/result_object/animation_utils.dart
@@ -3,12 +3,12 @@ import 'package:flutter/material.dart';
 
 
 class AnimationUtils {
-  static const int updateItemDurationInMilliseconds = 1000;
+  static const int updateItemDurationInMilliseconds = 1500;
   static AnimatedWidget getUpdateItemAnimation(Widget child, Animation<double> parent) {
     return SlideTransition(
         child: child,
         position: Tween<Offset>(
-            begin: Offset(-1, 0.25),
+            begin: Offset(0, 1),
             end: Offset(0, 0)
         ).animate(
             CurvedAnimation(curve: Curves.elasticInOut, parent: parent)
